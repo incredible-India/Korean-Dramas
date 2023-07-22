e0326cf444311da8a87e704ec56794ce6c182272
namya <namyahs004@gmail.com>
2023-07-22 22:31:10 +0530

    added actor controller

---
+++ Korean API/Korean Api/Controllers/ActorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Korean_Api.Controllers
+{
+    [Route("api/actor")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+
+    }
+}
