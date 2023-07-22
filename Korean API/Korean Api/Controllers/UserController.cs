c12dbcd08b06e9a5183d7b9db3abf550ee32bfbf
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-07-22 22:28:02 +0530

    Added models ,interface and implemantaion folder

---
+++ Korean API/Korean Api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Korean_Api.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+
+    }
+}
