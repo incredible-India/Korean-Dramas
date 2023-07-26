96f8b5c6c77735141f08f800ea7c61ebdac5a728
namya <namyahs004@gmail.com>
2023-07-26 22:34:50 +0530

    Added User login and fav endpoint

--- Korean API/Korean Api/Interface/IUsers.cs
+++ Korean API/Korean Api/Interface/IUsers.cs
@@ -6,5 +6,9 @@ namespace Korean_Api.Interface
     {
         //Registration of the new user
         public Dictionary<string, string> NewUserRegistration(Users user);
+
+        //login user
+
+        public dynamic Login(string email, string password );
     }
 }
