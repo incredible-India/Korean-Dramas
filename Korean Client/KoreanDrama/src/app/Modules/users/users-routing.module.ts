0f3b621579028f926f575f2a9b8ab3791b1989e4
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-08-04 22:25:22 +0530

    added login form

--- Korean Client/KoreanDrama/src/app/Modules/users/users-routing.module.ts
+++ Korean Client/KoreanDrama/src/app/Modules/users/users-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewUserComponent } from './new-user/new-user.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', component:NewUserComponent}
+  {path: '', component:NewUserComponent},
+  {path: 'login', component:LoginComponent},
 ];
 
 @NgModule({
--- Korean Client/KoreanDrama/src/app/Modules/users/users.module.ts
+++ Korean Client/KoreanDrama/src/app/Modules/users/users.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import { UsersRoutingModule } from './users-routing.module';
 import { NewUserComponent } from './new-user/new-user.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    NewUserComponent
+    NewUserComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
