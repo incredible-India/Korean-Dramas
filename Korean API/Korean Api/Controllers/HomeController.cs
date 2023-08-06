8a94d28b3dfe76047809ae239d77fbff54010000
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-08-06 15:53:02 +0530

    added add actor and user

--- Korean API/Korean Api/Controllers/HomeController.cs
+++ Korean API/Korean Api/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Korean_Api.Interface;
+using Korean_Api.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Korean_Api.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IActor _actor;
+
+        public HomeController(IActor actor)
+        {
+          this._actor  =    actor;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,6 +19,27 @@ namespace Korean_Api.Controllers
 
         //adding the Actor
         public IActionResult AddActor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //adding the Actor
+        public IActionResult AddActor(LeadActors actor)
+        {
+
+            var a = _actor.NewActor(actor);
+            
+            if(a != null)
+            {
+                TempData["success"] = $"{actor.Name} added successfully..";
+            }
+            return RedirectToAction("AddActor");
+        }
+
+
+        //adding the user 
+        public IActionResult AddUser()
         {
             return View();
         }
